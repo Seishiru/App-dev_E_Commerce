c554a9834dda4de8cf4d44aa8dbd4d292187af2b
Roxanne Lopez <roxanneaggielopez@gmail.com>
2024-11-30 17:26:33 +0800

    Create initial structure of cart page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "../css/CartPage.css";
 
 function CartPage() {
   return (
-    <div>CartPage</div>
-  )
+    <div className="container">
+      <div className="section-container cart-columns">
+        <div></div>
+        <div><h3 className="cart-column-name">Product</h3></div>
+        <div><h3 className="cart-column-name">Unit Price</h3></div>
+        <div><h3 className="cart-column-name">Quantity</h3></div>
+        <div><h3 className="cart-column-name">Total Price</h3></div>
+        <div></div>
+      </div>
+
+      <div className="section-container cart-container">
+        <div className="cart-list">
+          <div className="cart-columns cart-item">
+            <div>checkbox</div>
+            <div>image+productname</div>
+            <div>price</div>
+            <div>quantity</div>
+            <div>total</div>
+            <div>cancel</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CartPage
+export default CartPage;
