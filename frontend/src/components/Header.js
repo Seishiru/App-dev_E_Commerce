3bd4e22f86752a04f336b3520f74655ed58b7169
Roxanne Lopez <roxanneaggielopez@gmail.com>
2024-11-30 21:12:53 +0800

    Add cart page UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,30 +7,34 @@ import Logo from "../assets/Logo.png"; // Import the logo
 const Header = ({ setShowLogin, setShowSignup }) => {
   return (
     <div className="header-container">
-      <div className="header-left">
-      <img src={Logo} alt="Shopee Logo" className="logo" />
-      <h1 className="site-name">Shopease</h1>
-
-      {/* Search Bar and Button */}
-      <div className="search-bar">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search for products..."
-        />
-        <button className="search-button">
-          <i className="fas fa-search"></i>
-        </button>
-        <div className="cart">
-          <Link to="/cart">
-            <i className="fas fa-shopping-cart cart-icon"></i>
-          </Link>
-        </div>
+      <div className="header-section">
+        <img src={Logo} alt="Shopee Logo" className="logo" />
+        <h1 className="site-name">Shopease</h1>
       </div>
+
+      <div className="header-section">
+        {/* Search Bar and Button */}
+        <div className="search-bar">
+          <div>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for products..."
+          />
+          </div>
+          <button className="search-button">
+            <i className="fas fa-search"></i>
+          </button>
+          <div className="cart">
+            <Link to="/cart">
+              <i className="fas fa-shopping-cart cart-icon"></i>
+            </Link>
+          </div>
+        </div>
       </div>
 
       {/* Header Buttons */}
-      <div className="header-right">
+      <div className="header-section">
         <button className="header-button" onClick={() => setShowLogin(true)}>
           Login
         </button>
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,29 +1,76 @@
 import React from "react";
+import { useState } from "react";
 import "../css/CartPage.css";
+import empty from "../assets/empty_image.png";
 
 function CartPage() {
+  const [quantity, setQuantity] = useState(1);
+  const price = 100
+
+  const handleIncrease = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      setQuantity(prevQuantity => prevQuantity - 1);
+    }
+  };
+
   return (
     <div className="container">
       <div className="section-container cart-columns">
         <div></div>
-        <div><h3 className="cart-column-name">Product</h3></div>
-        <div><h3 className="cart-column-name">Unit Price</h3></div>
-        <div><h3 className="cart-column-name">Quantity</h3></div>
-        <div><h3 className="cart-column-name">Total Price</h3></div>
+        <div>
+          <h3 className="cart-column-name">Product</h3>
+        </div>
+        <div>
+          <h3 className="cart-column-name">Unit Price</h3>
+        </div>
+        <div>
+          <h3 className="cart-column-name">Quantity</h3>
+        </div>
+        <div>
+          <h3 className="cart-column-name">Total Price</h3>
+        </div>
         <div></div>
       </div>
 
       <div className="section-container cart-container">
         <div className="cart-list">
           <div className="cart-columns cart-item">
-            <div>checkbox</div>
-            <div>image+productname</div>
-            <div>price</div>
-            <div>quantity</div>
-            <div>total</div>
-            <div>cancel</div>
+            <div className="icon-container">
+              <input type="checkbox" className="custom-checkbox" />
+            </div>
+            <div className="cart-product-container">
+              <img src={empty} className="cart-product-image" />
+              <div className="cart-product-name">
+                <h3>Product name</h3>
+                <div>variation</div>
+              </div>
+            </div>
+            <div>₱{price.toFixed(2)}</div>
+            <div className="cart-quantity-container">
+              <button className="quantity-btn" onClick={() => handleDecrease()}>-</button>
+              <input
+                type="text"
+                className="quantity-input"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                disabled
+              />
+              <button className="quantity-btn" onClick={() => handleIncrease()}>+</button>
+            </div>
+            <div>₱{(quantity*price).toFixed(2)}</div>
+            <div className="icon-container">
+              <i className="fa-solid fa-trash delete-icon"></i>
+            </div>
           </div>
         </div>
+      </div>
+
+      <div className="checkout-container">
+        <button className="checkout-button">Checkout</button>
       </div>
     </div>
   );
