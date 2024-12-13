de86984e7be073ae22eaf55626495e7c4e7ee872
Roxanne Lopez <roxanneaggielopez@gmail.com>
2024-12-13 12:06:03 +0800

    Add styles for product card

--- frontend/src/components/ProductItem.js
+++ frontend/src/components/ProductItem.js
@@ -1,14 +1,20 @@
 import React from "react";
-import '../css/ProductItem.css'
-import { Link } from "react-router-dom"; 
+import "../css/ProductItem.css";
+import { Link } from "react-router-dom";
 
 function ProductItem() {
   return (
-    <Link to='/product'>
+    <Link to="/product">
       <div className="product-card">
-      <div className="product-image">image</div>
-      <div className="product-description">Product 5 description</div>
-    </div>
+        <div className="product-card-image">image</div>
+        <div className="product-card-summary">
+          <div className="product-card-name">Product name</div>
+          <div className="product-card-details">
+            <div className="product-card-price">₱100</div>
+            <div>10k+ sold</div>
+          </div>
+        </div>
+      </div>
     </Link>
   );
 }
