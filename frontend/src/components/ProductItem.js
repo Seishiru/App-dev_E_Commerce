ab18ce36057a6673ae33bcc627a3dbbbe5052fc2
Oni-sama <hayatominmin@gmail.com>
2024-12-14 14:22:07 +0800

    Uploads products

--- frontend/src/components/ProductItem.js
+++ frontend/src/components/ProductItem.js
@@ -7,10 +7,11 @@ function ProductItem({ product }) {
     <Link to={`/product/${product.product_id}`}>
       <div className="product-card">
         <div className="product-card-image">
-          <img src={product.image_url} alt={product.name} />
+          <img src={`http://localhost:5000/uploads/${product.image_url}`} alt={product.name} />
         </div>
         <div className="product-card-summary">
           <div className="product-card-name">{product.name}</div> {/* Display name */}
+          <div className="product-card-description">{product.description}</div>
           <div className="product-card-details">
             <div className="product-card-price">₱{product.price}</div>
             <div>{product.stock_quantity} in stock</div>
